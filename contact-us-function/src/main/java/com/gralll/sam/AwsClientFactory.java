607cc7d9a33b5d37256bde0f87dcbe442d103b3a
Gralll <aleksandr.gruzdev94@gmail.com>
2020-02-09 18:26:25 +0300

    Small refactoring.

--- contact-us-function/src/main/java/com/gralll/sam/AwsClientFactory.java
+++ contact-us-function/src/main/java/com/gralll/sam/AwsClientFactory.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * Just util class for eager initialization of sdk clients.
+ * Just an util class for an eager initialization of sdk clients.
  */
 public class AwsClientFactory {
 
